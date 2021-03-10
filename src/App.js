e3fba2cf61f2f7874dff3b6b54345177b61db40e
Roman <38030587+romanich141@users.noreply.github.com>
2021-03-10 19:21:17 +0200

    + useHover add hook useHover

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import React, { useRef } from "react";
 import useInput from "../custom-hooks/useInput";
-import "./styles.css";
+import useHover from "../custom-hooks/useHover";
+import "./styles.scss";
 
 const App = () => {
-  const [user, setUser] = useInput("");
-  const [password, setPassword] = useInput("");
+  // const [user, setUser] = useInput("");
+  // const [password, setPassword] = useInput("");
+  // const ref = useRef();
+  // const isHoverBox = useHover(ref);
 
   return (
     <div className="App">
-      <div>{user || "user"}</div>
+      {/* <div>{user || "user"}</div>
       <div>{password || "password"}</div>
       <div>
         <input value={user} onChange={setUser} />
       </div>
       <div>
         <input value={password} onChange={setPassword} />
-      </div>
+      </div> */}
+
+      {/* <div
+        ref={ref}
+        className={`box ${isHoverBox ? "box__orange" : "box__green"} `}
+      /> */}
     </div>
   );
 };
